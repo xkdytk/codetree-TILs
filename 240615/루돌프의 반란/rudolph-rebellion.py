c500e4c3c0109d536e5557c06a20b2b715102c15
xkdytk <57613321+xkdytk@users.noreply.github.com>
2024-06-15 21:49:54 +0900

    Title: 루돌프의 반란, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240615/루돌프의 반란/rudolph-rebellion.py
+++ 240615/루돌프의 반란/rudolph-rebellion.py
@@ -1,130 +1,132 @@
-def inter(py, px, dy, dx):
-    pid = A[py][px]
-    stack = [(py, px, pid)]
+def inter(y, x, dir, id):
+    nid = A[y][x]
+    A[y][x] = id
+    santa[id] = (y, x)
     
-    while stack:
-        py, px, pid = stack.pop(0)
-        npy, npx = py+dy, px+dx
-        if 0<=npy<N and 0<=npx<N:
-            if A[npy][npx] != 0:
-                stack.append((npy, npx, A[npy][npx]))
-            A[npy][npx] = pid
-            santa[pid] = (npy, npx)
+    ny, nx = y+dir[0], x+dir[1]
+    if 0<=ny<N and 0<=nx<N:
+        if A[ny][nx] != 0:
+                inter(ny, nx, dir, nid)
         else:
-            fail[pid] = True
+            A[ny][nx] = nid
+            santa[nid] = (ny, nx)
     
-    return
+    else:
+        fail[id] = True
     
+    return    
 
-def r_move(r_pos, c, turn):
-    ry, rx = r_pos
-    near = []
-    dist = 3*N**2
-    
-    for i in range(1, len(santa)):
+def rmove(turn):
+    global ry, rx
+    dist = 2*(P**2)
+    select = []
+    for i in range(1, P+1):
         if not fail[i]:
-            now = (ry-santa[i][0])**2 + (rx-santa[i][1])**2
-            if dist > now:
-                dist = now
-                near = [(santa[i][0], santa[i][1], i)]
-            elif dist == now:
-                near.append((santa[i][0], santa[i][1], i))
+            sy, sx = santa[i]
+            nd = (ry-sy)**2 + (rx-sx)**2
+            if nd < dist:
+                dist = nd
+                select = [(sy, sx, i)]
+            elif nd == dist:
+                select.append((sy, sx, i))
+                
+    select.sort(reverse=True)
+    sy, sx, id = select[0]
     
-    near = sorted(near, reverse=True)
-    sy, sx, id = near[0]
-    
-    dy, dx = 0, 0
-    if sy > ry: dy = 1
-    elif sy < ry: dy = -1
-    
-    if sx > rx: dx = 1
-    elif sx < rx: dx = -1
-        
+    dist = (ry-sy)**2 + (rx-sx)**2
+    dir = (0, 0)
+    for d in ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, 1), (1, -1)):
+        if 0<=ry+d[0]<N and 0<=rx+d[1]<N:
+            nd = (ry+d[0]-sy)**2 + (rx+d[1]-sx)**2
+            if nd < dist:
+                dist = nd
+                dir = d
+
     A[ry][rx] = 0
-    nry, nrx = ry+dy, rx+dx
-    A[nry][nrx] = -1
-    if (nry, nrx) == (sy, sx):
-        result[id] += c
-        stun[id] = turn + 2
-        py, px = sy+c*dy, sx+c*dx
-        santa[id] = (py, px)
-        if 0<=py<N and 0<=px<N:
-            if A[py][px] != 0:
-                inter(py, px, dy, dx)
-            A[py][px] = id
+    ry, rx = ry+dir[0], rx+dir[1]   
+    A[ry][rx] = -1  
+    if (sy, sx) == (ry, rx):
+        result[id] += C
+        stun[id] = turn+2
+        cy, cx = sy+dir[0]*C, sx+dir[1]*C
+        santa[id] = (cy, cx)
+        if 0<=cy<N and 0<=cx<N:
+            if A[cy][cx] != 0:
+                inter(cy, cx, dir, id)
+            else:
+                A[cy][cx] = id
         else:
             fail[id] = True
-            
-    return (nry, nrx)
+        
+    return    
 
-def s_move(r_pos, d, turn):
-    ry, rx = r_pos
-    for i in range(1, len(santa)):
-        sy, sx = santa[i][0], santa[i][1]
-        if not fail[i]:
-            if stun[i] > turn:
+def smove(turn):
+    for i in range(1, P+1):
+        if stun[i] <= turn and not fail[i]:
+            sy, sx = santa[i]
+            dist = (ry-sy)**2 + (rx-sx)**2
+            dir = (0, 0)
+            for dy, dx in ((-1, 0), (0, 1), (1, 0,), (0, -1)):
+                ny, nx = sy+dy, sx+dx
+                if 0<=ny<N and 0<=nx<N and (A[ny][nx] == 0 or A[ny][nx] == -1):
+                    nd = (ry-ny)**2 + (rx-nx)**2
+                    if nd < dist:
+                        dist = nd
+                        dir = (dy, dx)
+                        
+            if dir == (0, 0):
                 continue
             else:
-                tlst = []
-                dist = (ry-sy)**2 + (rx-sx)**2
-                for dy, dx in ((-1, 0), (0, 1), (1, 0), (0, -1)): # 거리 리스트로 관리하면 편함
-                    nsy, nsx = sy+dy, sx+dx
-                    if 0<=nsy<N and 0<=nsx<N and A[nsy][nsx] <= 0:
-                        now = (ry-nsy)**2 + (rx-nsx)**2
-                        if dist > now:
-                            dist = now
-                            tlst = [(nsy, nsx, dy, dx)]
-                            
-                if len(tlst) == 0: continue
-                else:
-                    A[sy][sx] = 0
-                    nsy, nsx, dy, dx = tlst[0]
-                    if (nsy, nsx) == (ry, rx):
-                        dy, dx = -dy, -dx
-                        result[i] += d
-                        stun[i] = turn + 2
-                        py, px = nsy+d*dy, nsx+d*dx
-                        santa[i] = (py, px)
-                        if 0<=py<N and 0<=px<N:
-                            if A[py][px] != 0:
-                                inter(py, px, dy, dx)
-                            A[py][px] = i
+                A[sy][sx] = 0
+                sy, sx = sy+dir[0], sx+dir[1]  
+                santa[i] = (sy, sx)
+                if (sy, sx) == (ry, rx):
+                    result[i] += D
+                    stun[i] = turn+2
+                    dir = (-dir[0], -dir[1])
+                    cy, cx = sy+dir[0]*D, sx+dir[1]*D
+                    santa[i] = (cy, cx)
+                    if 0<=cy<N and 0<=cx<N:
+                        if A[cy][cx] != 0:
+                            inter(cy, cx, dir, i)
                         else:
-                            fail[i] = True
+                            A[cy][cx] = i
                     else:
-                        santa[i] = (nsy, nsx)
-                        A[nsy][nsx] = i
-       
-def end_check():
-    if fail.count(True) == P+1:
-        return True
-def plus():
-    for id in range(1, len(santa)):
-        if not fail[id]:
-            result[id] += 1
+                        fail[i] = True
+                else:
+                    A[sy][sx] = i
+                    
+    return
                 
-N, M, P, C, D = map(int, input().split())
-r_y, r_x = map(int, input().split())
-r_pos = (r_y-1, r_x-1) # 줄여서 편한것은 줄이고 원래 값을 유지해야하는 것은 유지 (이후에 헷갈리지 않도록 주의)
+                
+            
 
-santa = [(0,0) for _ in range(P+1)]
-stun = [1 for _ in range(P+1)]
-fail = [False for _ in range(P+1)]
-fail[0] = True
+N, M, P, C, D = map(int, input().split())
 
 A = [[0 for _ in range(N)] for _ in range(N)]
-A[r_pos[0]][r_pos[1]] = -1
+
+ry, rx = map(int, input().split())
+ry, rx = ry-1, rx-1
+A[ry][rx] = -1
+santa = [(-1, -1) for _ in range(P+1)]
 for _ in range(P):
-    num, y, x = map(int, input().split())
-    santa[num] = (y-1, x-1)
-    A[y-1][x-1] = num
+    i, y, x = map(int, input().split())
+    santa[i] = (y-1, x-1)
+    A[y-1][x-1] = i
     
+
+fail = [False for _ in range(P+1)]
+fail[0] = True
+stun = [0 for _ in range(P+1)]
 result = [0 for _ in range(P+1)]
+
 for turn in range(1, M+1):
-    if end_check():
+    rmove(turn)
+    smove(turn)
+    if fail.count(True) == P+1:
         break
-    r_pos = r_move(r_pos, C, turn)
-    s_move(r_pos, D, turn)
-    plus()
-    
+    for i in range(1, len(fail)):
+        if not fail[i]:
+            result[i] += 1
+            
 print(*result[1:])
